34ae94b07262d60c143def5ee43e970498c9473f
SidTheChillGuy <sidumahajan@gmail.com>
2025-02-26 22:24:52 +0530

    toast toggles, fix sumeet data, manifest data

--- app/src/main/java/com/epics/patientconnect/LandingUserPage.kt
+++ app/src/main/java/com/epics/patientconnect/LandingUserPage.kt
@@ -1,20 +1,44 @@
 package com.epics.patientconnect
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class LandingUserPage : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_landing_user_page)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val button1: Button = findViewById(R.id.medicalhistbtn)
+        val button2: Button = findViewById(R.id.uploadreportsbtn)
+        val button3: Button = findViewById(R.id.findhospitalsbtn)
+
+        button1.setOnClickListener {
+            showmedicalhist()
         }
+
+        button2.setOnClickListener {
+            uploadreports()
+        }
+
+        button3.setOnClickListener {
+            findhospitals()
+        }
+    }
+    private fun showmedicalhist()
+    {
+        val medhist = Intent(this, UserHistoryPage::class.java)
+        startActivity(medhist)
+    }
+    private fun uploadreports()
+    {
+        Toast.makeText(this, "UploadReports Not Implemented", Toast.LENGTH_LONG).show()
+    }
+    private fun findhospitals()
+    {
+        Toast.makeText(this, "FindHospitals not Implemented", Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/com/epics/patientconnect/UserHistoryPage.kt
+++ app/src/main/java/com/epics/patientconnect/UserHistoryPage.kt
@@ -34,15 +34,12 @@ class UserHistoryPage : AppCompatActivity() {
 
     private fun setData()
     {
-        UserPageCaseIDtitleList = listOf("Case: 0001TEST", "Case: 0002VITB", "Case: 0003", "Case: 0004")
+        UserPageCaseIDtitleList = listOf("Case: 0001", "Case: 0002")
 
         UserPageCaseIDdataList = java.util.HashMap()
 
-        // ayush saxena update this !!!
-        UserPageCaseIDdataList[UserPageCaseIDtitleList[0]] = listOf("Detail 1: Bukhaar")
-        UserPageCaseIDdataList[UserPageCaseIDtitleList[1]] = listOf("Detail 1: khansi","Detail 2: TB hai")
-        UserPageCaseIDdataList[UserPageCaseIDtitleList[2]] = listOf("Detail 1: liver gayab","Detail 2: daaru peeta hai","Detail 3: admit krdo aa jayegi")
-        UserPageCaseIDdataList[UserPageCaseIDtitleList[3]] = listOf("Detail 1: khoon kharab","Detail 2: nasha nhi krta","Detail 3: admit toh kia hai","Detail 4: cancer T~T")
+        UserPageCaseIDdataList[UserPageCaseIDtitleList[0]] = listOf("\tDate:15 February 2025\nReport:\nLeg Fracture:\n\t- Requested Xray")
+        UserPageCaseIDdataList[UserPageCaseIDtitleList[1]] = listOf("\tDate:16 February 2025\nReport:\nCrack at Foot base\nRecommended rest for 40days")
     }
 
 
